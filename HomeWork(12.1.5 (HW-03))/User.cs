7f09be428dc1568c98810ed1c1d70efa4d062583
Mike <meke2001@mail.ru>
2024-04-25 23:04:03 +0300

    Добавьте файлы проекта.

---
+++ HomeWork(12.1.5 (HW-03))/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_12._1._5__HW_03__
+{
+
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+
+    }
+}
